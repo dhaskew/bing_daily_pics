8822ea1a2851eaac2d40ad251031236c6ebc6968
David Askew <davidaskew@gmail.com>
2012-10-27 14:16:25 -0400

    Changed from growl-notify to terminal-notifier

--- bing_1.rb
+++ bing_1.rb
@@ -4,8 +4,8 @@ require 'rss'
 require 'open-uri'
 require 'logger'
 
-require 'ruby-growl'
-require 'logger/colors'
+require 'logger/colors'      #gem install logger-colors
+require 'terminal-notifier'  #gem install terminal-notifier
 
 # useful documentation
 # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/rss/rdoc/RSS/Parser.html
@@ -19,25 +19,18 @@ require 'logger/colors'
 @log.level = Logger::DEBUG # DEBUG,INFO,WARN,ERROR,FATAL
 
 # alert on program start
-g1_result = `growl -H localhost -t "Bing Pic of the Day" -m "Refresh Started" -n "Ruby Script : Bing Picture of the Day"`
+TerminalNotifier.notify("Bing: Refresh Started", :activate => 'activate test', :title => "Bing Pic of the Day")
 
 enclosures = []
 
 # the location to download the files to
 downloads_dir = "/Users/dhaskew/pictures/bing_pics/"
 
-#if(downloads_dir == "/Users/dhaskew/pictures/bing_pics/")
-#  @log.fatal "You must change the default download directory to something more appropriate!"
-#  exit 0
-#end
 
 # create directory if it doesn't exist
 if(!File.exists?(downloads_dir))
   @log.fatal "The folder specified in downloads_dir does not exist! Please create and rerun script."
   exit 0
-  #puts "creating directory #{downloads_dir}"
-  #Dir.mkdir(downloads_dir)
-  #puts "directory created"
 end
 
 @log.debug "RSS fetch and parse : START"
@@ -45,18 +38,13 @@ end
 url = 'http://feeds.feedburner.com/bingimages'
 open(url) do |rss|
   feed = RSS::Parser.parse(rss,do_validate=false)
-  #puts "Title: #{feed.channel.title}"
   feed.items.each do |item|
-    #puts "Item: #{item.title}"
-    #puts "Image Link: #{item.link}"
-    #puts "Enclosure Link: #{item.enclosure.url}"
     enclosures << item.enclosure.url
   end
 end
 
 @log.debug "RSS fetch and parse : END" 
 
-#puts enclosures
 
 new_pictures = false
 
@@ -65,9 +53,6 @@ enclosures.each do |file|
   fname = file[file.index('%2f')+3,file.length]
   
   output_file = downloads_dir + fname
-  
-  #puts fpart1
-  #puts fname
   
   if(!File.exists?(output_file))
   
@@ -87,10 +72,10 @@ end
 
 # alert on status of pictures
 if(new_pictures)
-  g1_result = `growl -H localhost -t "Bing Pic of the Day" -m "New Pictures Available" -n "Ruby Script : Bing Picture of the Day"`
+  TerminalNotifier.notify("Bing: New Pictures Available", :activate => 'activate test', :title => "Bing Pic of the Day")
 else
-  g1_result = `growl -H localhost -t "Bing Pic of the Day" -m "No New Pictures Downloaded" -n "Ruby Script : Bing Picture of the Day"`
+  TerminalNotifier.notify("Bing: No New Pictures", :activate => 'activate test', :title => "Bing Pic of the Day")
 end  
 
 # alert when program done
-g1_result = `growl -H localhost -t "Bing Pic of the Day" -m "Refresh Complete" -n "Ruby Script : Bing Picture of the Day"`
+TerminalNotifier.notify("Bing: Refresh Complete", :activate => 'activate test', :title => "Bing Pic of the Day")
